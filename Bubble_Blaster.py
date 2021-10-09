eeb997a7f5cffa5511228d2cfbda24a0da2b7de4
Binula Kavisinghe <81571678+Binula-Kavisinghe@users.noreply.github.com>
2021-10-09 08:18:33 +0530

    Update Bubble_Blaster.py

--- Bubble_Blaster.py
+++ Bubble_Blaster.py
@@ -1,1 +1,8 @@
-print ("Still in the works")
+from tkinter import * 
+HEIGHT = 500
+WIDTH = 800
+window = Tk()
+window.title ("Bubble Blaster")
+c = Canvas(window, width = WIDTH, height = HEIGHT, bg = "darkblue")
+c.pack()
+
